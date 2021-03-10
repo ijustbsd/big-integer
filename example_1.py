3457d83439a58b8ce1d1cb5c5e3a13687f9e4bed
Sergey Baboshin <ijustbsd@gmail.com>
2021-03-10 13:46:46 +0300

    Add example_1

---
+++ example_1.py
@@ -0,0 +1,13 @@
+from bigint import BigInt
+
+x = BigInt('-99999999999999999999')
+y = BigInt('11111111111111111111')
+
+print('x =', x)
+print('y =', y)
+
+print('x + y =', x + y)
+print('x - y =', x - y)
+print('x * y =', x * y)
+print('x / y =', x / y)
+print('x mod y =', x % y)
